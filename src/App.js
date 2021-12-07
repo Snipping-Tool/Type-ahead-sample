2bb3196a75ded6ae1cbd3da83b83d46c6f695756
carvzzz_32 <carveylamosao123@gmail.com>
2021-12-07 21:40:09 +0800

    added images and additional UI adjustments

--- src/App.js
+++ src/App.js
@@ -5,13 +5,25 @@ import TypeAheadCard from './components/cards/TypeAheadCard.js'
 export default function App() {
   return (
     <>
-    <div className="App">
-      <header className="App-header">
-        Github API:  Users (Typeahead Sample)
-        <p>by Snipping-Tool</p>
-      </header>
-      <TypeAheadCard/>
-    </div>
+    {/* <div className="App"> */}
+      <div className="AppBackground"
+        style={{
+          backgroundImage: 'url(' + require('./assets/images/pattern3.png').default + ')',
+        }}
+      >
+        <header className="App-header">
+          Github API: User Search Box
+          <p>Typeahead Sample</p>
+        </header>
+        <div className="CardContainer">
+          <TypeAheadCard/>
+          
+        </div>
+      </div>
+    {/* </div> */}
+    <footer className="AppFooter">
+        <p>Made by <a href="http://github.com/Snipping-Tool">Carvey Lamosao </a>© 2021</p>
+    </footer>
     </>
   );
 }
